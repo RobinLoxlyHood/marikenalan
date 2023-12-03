86df5cdb6858d3eaef4ec41e798886c5e9386e1e
RobinLoxlyHood <iki11111998@gmail.com>
2023-12-03 23:09:03 +0700

    Markintal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HomeComponent,NgbModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
+  
 })
+
 export class AppComponent {
-  title = 'Web-profil';
+  
+  title = 'homes';
+
 }
+
+
